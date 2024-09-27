34251ad7e6d77e55b8ca0af5f91738f538499959
hemgaard <pehem22@student.sdu.dk>
2024-09-27 13:18:57 +0200

    latest

--- Task4/Task4/Program.cs
+++ Task4/Task4/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Task4{
+public class Program{
+
+    /*
+    Write a function MultipliedNumber this method should take in a parameter
+    the parameter needs to be an int. The function should also return an int.
+    The paramter int should be multiplied by 2 and returned.
+    */
+
+    //Write your solution between the dotted lines!
+			//--------------------------------------------
+    public static int ForLoop(int n){
+        for(int i = 0; i < n; i++){
+            return n;
+        }
+    }
+   
+			//--------------------------------------------
+
+    public static void Main(string[] args){
+        Console.WriteLine(Program.ForLoop(10));
+
+    }
+
+    }
+}
