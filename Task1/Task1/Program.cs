efba484d27a1d85c6115e2a2e0a321a0f3357800
Marcus <marcusbthomsen@gmail.com>
2024-09-27 12:42:06 +0200

    First task & fixed gitignore

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -1,33 +1,24 @@
 ﻿namespace Task1 {
 	public class Program {
-		public static void Main(string[] args) {
-			
+		public static void Main(string[] args) {}
 
-			/*
-			[ TASK 1 ]
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-			sed do eiusmod tempor incididunt ut labore et dolore magna 
-			aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-			ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-			Duis aute irure dolor in reprehenderit in voluptate velit esse 
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
-			cupidatat non proident, sunt in culpa qui officia deserunt mollit 
-			anim id est laborum.
-			*/
+		/*
+		TASK 1 - Sum of array
+		Below is a function that returns an integer and takes an integer 
+		array as its parameter.
 
+		Within the body of the function, write code that sums up all the
+		values in the array and returns that sum. (i.e. add all the integers
+		together and return the result).
+
+		*/
+
+		public static int SumOfArray(int[] arr) {
 			//Write your solution between the dotted lines!
 			//--------------------------------------------
 
 
 			//--------------------------------------------
-
-			Console.WriteLine("Hello, World!");
-		}
-
-		public static string SomeFunctionToImplement(string str, int i) {
-			//WIP - Some task within a function goes here
-
-			return null;
-		}
+		}        
 	}
 }
